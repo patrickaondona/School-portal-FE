bab35fca703464ddd96fb181214f2016a25b9eb5
21ntphilos <ucheokoye99@gmail.com>
2025-05-05 14:37:00 +0100

    Setup II

--- README.md
+++
@@ -1,1 +0,0 @@
-"# School-portal-FE" 
--- School-portal-FE/src/routes/routes.tsx
+++ School-portal-FE/src/routes/routes.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom"
-
 import Layout from "./layout"
 import ErrorPage from "../pages/ErrorPage"
 import App from "../App"
